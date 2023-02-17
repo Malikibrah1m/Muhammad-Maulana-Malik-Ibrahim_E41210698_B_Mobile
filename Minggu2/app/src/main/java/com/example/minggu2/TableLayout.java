c9f76de7bf876c7778ce51369a394862fc4a2b20
Malikibrah1m <malikcitizen@gmail>
2023-02-17 08:03:17 +0700

    Scroll view

--- Minggu2/app/src/main/java/com/example/minggu2/TableLayout.java
+++ Minggu2/app/src/main/java/com/example/minggu2/TableLayout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TableLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.horizontalscroll);
+        setContentView(R.layout.activity_table_layout);
     }
 }
